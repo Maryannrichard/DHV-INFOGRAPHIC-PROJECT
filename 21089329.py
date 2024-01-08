78b48f18262dc8efa6a472444d77b2a183198975
Maryann <maryannrichard212@gmail.com>
2024-01-08 22:59:48 +0000

    read the data

---
+++ 21089329.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 22:26:25 2024
+
+@author: A
+"""
+
+# importing necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# reading te data.
+df = pd.read_excel('World_Development_Indicators(9).xlsx')
+
